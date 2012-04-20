0d7885f352b667dd5b82898764f40d58123c7aa1
Patrick Grennan <grennan@nyu.edu>
2012-04-20 10:39:14 -0400

    Initial Commit

---
+++ singSong/PTGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PTGAppDelegate.h
+//  singSong
+//
+//  Created by Patrick Grennan on 4/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PTGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ singSong/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  singSong
+//
+//  Created by Patrick Grennan on 4/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PTGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PTGAppDelegate class]));
+    }
+}
